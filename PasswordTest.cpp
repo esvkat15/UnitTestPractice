6522cf1cbb981cdc4b6fdf3d1307222ddf7558e3
esvkat15 <matthewhmunoz@gmail.com>
2025-04-16 12:35:27 -0700

    Create initial unit tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,27 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, two_leading_a)
+{
+	Password my_password;
+	ASSERT_EQ( 2, my_password.count_leading_characters("aab") );
+}
+
+TEST(PasswordTest, cap_low_stuff)
+{
+	Password my_password;
+	ASSERT_EQ( 1, my_password.count_leading_characters("Aab") );
+}
+
+TEST(PasswordTest, low_cap_stuff)
+{
+	Password my_password;
+	ASSERT_EQ( 1, my_password.count_leading_characters("aAb") );
+}
+
+TEST(PasswordTest, empty_str)
+{
+	Password my_password;
+	ASSERT_EQ( 0, my_password.count_leading_characters("") );
+}
